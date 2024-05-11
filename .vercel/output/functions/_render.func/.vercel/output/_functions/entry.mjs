0f983fa067ff2c84371948050baaac4467916b4c
Proyecto Madagascar Films <145238589+MadagascarFilms@users.noreply.github.com>
2024-05-10 21:14:52 -0500

    Actualizada la versión de Node.js en el archivo de configuración de Vercel y el archivo de entrada de funciones

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,5 +1,5 @@
 import { renderers } from './renderers.mjs';
-import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_D6bG-LGG.mjs';
+import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_iUeNbBdv.mjs';
 import { e as appendForwardSlash, j as joinPaths, t as trimSlashes, f as fileExtension, s as slash, p as prependForwardSlash, g as removeTrailingForwardSlash, h as collapseDuplicateSlashes } from './chunks/astro/assets-service_BYaRjSlY.mjs';
 import { R as ROUTE_TYPE_HEADER, l as REROUTE_DIRECTIVE_HEADER, A as AstroError, n as ResponseSentError, o as MiddlewareNoDataOrNextCalled, p as MiddlewareNotAResponse, G as GetStaticPathsRequired, q as InvalidGetStaticPathsReturn, t as InvalidGetStaticPathsEntry, u as GetStaticPathsExpectedParams, v as GetStaticPathsInvalidRouteParam, P as PageNumberParamNotFound, D as DEFAULT_404_COMPONENT, N as NoMatchingStaticPathFound, w as PrerenderDynamicEndpointPathCollide, x as ReservedSlotName, y as renderSlotToString, z as renderJSX, B as chunkToString, C as isRenderInstruction, H as LocalsNotAnObject, J as clientLocalsSymbol, K as clientAddressSymbol$1, O as ClientAddressNotAvailable, S as StaticClientAddressNotAvailable, Q as ASTRO_VERSION, T as responseSentSymbol$1, V as AstroResponseHeadersReassigned, W as renderPage, X as renderEndpoint, Y as REROUTABLE_STATUS_CODES } from './chunks/astro_COR_oCTK.mjs';
 import { serialize, parse } from 'cookie';
@@ -2163,7 +2163,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "28185306-0c5f-42ab-803a-717ed76958f1",
+    "middlewareSecret": "8d565878-925f-4a63-8630-71a8b0c435dc",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
